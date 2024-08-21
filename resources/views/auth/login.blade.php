4f90397426676da7f6222b5592d18e9505a0390e
nameoftheuser1 <angelo.jelo24@gmail.com>
2024-08-21 22:24:52 +0800

    attendance manual fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,14 +11,7 @@
 </style>
 <x-layout>
     <div class="fixed right-1/2 top-1/4 ">
-        <a href="{{ route('attendance') }}" class="flex mb-5 border-2 rounded-lg p-4 w-52 bg-gray-100 hover:bg-slate-300">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6 mr-2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-            <span>Attendance Manual</span>
-        </a>
+
     </div>
     <div class="flex bg-gray-100 rounded-lg overflow-hidden w-[800px]">
         <div class="flex-1 bg-cover bg-center" style="background-image: url('{{ asset('images/NASECURE.png') }}');">
@@ -27,8 +20,8 @@
             <form action="{{ route('login') }}" method="post">
                 @csrf
                 <label class="block text-center mb-2 font-bold" for="email">Email (@my.cspc.edu.ph)</label>
-                <input class="w-full p-1 mb-1 border border-gray-300 rounded-md" type="text"
-                    placeholder="Enter Email" value="{{ old('email') }}" id="email" name="email">
+                <input class="w-full p-1 mb-1 border border-gray-300 rounded-md" type="text" placeholder="Enter Email"
+                    value="{{ old('email') }}" id="email" name="email">
                 @error('email')
                     <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
                 @enderror
@@ -45,8 +38,12 @@
                     class="text-center no-underline block text-sm mt-2.5 hover:underline">Reset
                     Password</a>
                 <a href="{{ route('register') }}"
-                    class="text-center no-underline block text-sm mt-2.5 hover:underline">Don't have an account? <span
+                    class="text-center no-underline block text-sm mt-2.5 hover:underline mb-5">Don't have an account? <span
                         class="text-blue-700">Register here.</span></a>
+                <a href="{{ route('attendance') }}"
+                    class="text-center no-underline block text-sm mt-2.5 hover:underline text-blue-500">
+                    Click here for attendance manual.
+                </a>
             </form>
         </div>
     </div>
