e651903348c5b03b93769d6f6924d98bb8a9ed99
nameoftheuser1 <103398819+nameoftheuser1@users.noreply.github.com>
2024-08-29 20:16:38 +0800

    program-code removed

--- database/migrations/2024_07_10_093128_create_programs_table.php
+++ database/migrations/2024_07_10_093128_create_programs_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('programs', function (Blueprint $table) {
             $table->id();
             $table->string("program_name");
-            $table->string("program_code");
             $table->timestamps();
         });
     }
--- resources/views/programs/create.blade.php
+++ resources/views/programs/create.blade.php
@@ -14,15 +14,6 @@
                 @enderror
             </div>
             <div>
-                <label for="program_code" class="block text-sm font-medium text-gray-700">Program Code</label>
-                <input type="text" id="program_code" name="program_code"
-                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                    value="{{ old('program_code') }}">
-                @error('program_code')
-                    <p class="text-red-700 text-sm">{{ $message }}</p>
-                @enderror
-            </div>
-            <div>
                 <button type="submit"
                     class="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shasdow-sm text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                     Add Program
